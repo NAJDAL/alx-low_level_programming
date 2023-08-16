6486d5dd9255c1044b89372ac17503e12f0fae8a
NAJDAL <najdaleid6@gmail.com>
2023-08-16 04:22:09 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-int main(void)
-{
-	char school[10] = "_putchar";
+#include""main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-	int i;
 
-	for (i = 0; i < 8; i++)
+	int main(void)
 	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
